fedd977493cb1203e82c7ff5cc96224b6ac75bdb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-15 00:53:58 +0300

    Create 0004-Unused-variables.rs

---
+++ 0004-Unused-variables.rs
@@ -0,0 +1,14 @@
+// //Fix the warning below with :
+// fn main() {
+//     let x = 1; 
+// }
+
+// // Warning: unused variable: `x`
+
+#[allow(unused_variables)]
+
+fn main() {
+    let x = 1; 
+}
+
+// Warning: unused variable: `x`
